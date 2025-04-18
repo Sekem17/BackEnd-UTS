3d739cdbbc10a877ce698391ef922027048cd065
Samuel <samuel.535240044@stu.untar.ac.id>
2025-04-18 19:14:07 +0700

    Update schema

--- models/Monster.js
+++ models/Monster.js
@@ -1,15 +1,49 @@
 const mongoose = require('mongoose');
 
 const monsterSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    rarity: String,
-    health: Number,
-    defense: Number,
-    speed: Number,
-    challenge_rating: Number,
-    special_ability: String,
-    description: String
+    
+    name: {
+        type: String,
+        required: [true, 'Nama monster wajib diisi']
+    },
+    type: {
+        type: String,
+        required: [true, 'Tipe monster wajib diisi']
+    },
+    rarity: {
+        type: String,
+        enum: ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Mythic'],
+        required: [true, 'Rarity monster wajib diisi']
+    },
+    health: {
+        type: Number,
+        min: [1, 'Health minimal monster = 1']
+    },
+    defense: {
+        type: Number,
+        min: [1, 'Defemse minimal monster = 1'],
+    },
+    speed: {
+        type: Number,
+        min: [1, 'Speed minimal monster = 1'],
+    },
+    challenge_rating: {
+        type: Number,
+        min: [0, 'Challenge rating monster tidak boleh negatif']
+    },
+    special_ability: {
+        type: String,
+        required: [true, 'Special ability wajib diisi']
+    },
+    description: {
+        type: String,
+        maxlength: [500, 'Deskripsi monster maksimal 500 karakter']
+    },
+    num: {
+        type: Number,
+        required: true,
+        unique: true
+    }
 });
 
 module.exports = mongoose.model('Monster', monsterSchema);
