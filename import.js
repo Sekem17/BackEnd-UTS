636954fee976083582f75f1da80383faf45ba32c
Samuel <samuel.535240044@stu.untar.ac.id>
2025-04-23 23:55:07 +0700

    mengubah untuk import data awal classes

--- import.js
+++ import.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
-const Spell = require('./models/Spell');
+const Class = require('./models/Class');
 
 mongoose.connect('mongodb+srv://Samuel:popapopi17@contohcluster.27tfw.mongodb.net/?retryWrites=true&w=majority&appName=contohCluster', {
     useNewUrlParser: true,
@@ -8,10 +8,10 @@ mongoose.connect('mongodb+srv://Samuel:popapopi17@contohcluster.27tfw.mongodb.ne
 .then(async () => {
     console.log('Connected to MongoDB');
     
-    const data = fs.readFileSync('./data/spells.json');
-    const spells = JSON.parse(data);
+    const data = fs.readFileSync('./data/classes.json');
+    const classes = JSON.parse(data);
 
-    await Spell.insertMany(spells);
+    await Class.insertMany(classes);
     console.log('Data imported successfully');
 
     mongoose.disconnect();
