62567f0d3f34523428b44003388d33e442c64b3a
Samuel <samuel.535240044@stu.untar.ac.id>
2025-04-23 23:53:45 +0700

    update method put post dan delete

--- routes/weapons.js
+++ routes/weapons.js
@@ -25,4 +25,47 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+  // ini buat update spell berdasarkan ID
+  router.put('/:id', async (req, res) => {
+      try {
+          const updated = await Weapon.findByIdAndUpdate(
+              req.params.id,
+              req.body,
+              { new: true, runValidators: true }
+          );
+  
+          if (!updated) {
+              return res.status(404).json({ message: 'Weapon tidak ditemukan' });
+          }
+  
+          res.json(updated);
+      } catch (err) {
+          res.status(400).json({ message: err.message });
+      }
+  });
+  
+  //ini buat tambah spell baru
+  router.post('/', async (req, res) => {
+      const weapons = new Weapon(req.body);
+      try {
+          const saved = await weapons.save();
+          res.status(201).json(saved);
+      } catch (err) {
+          res.status(400).json({ message: err.message });
+      }
+  });
+  
+  // ini buat hapus spell berdasarkan ID
+  router.delete('/:id', async (req, res) => {
+      try {
+          const deleted = await Weapon.findByIdAndDelete(req.params.id);
+          if (!deleted) {
+              return res.status(404).json({ message: 'Weapon tidak ditemukan' });
+          }
+          res.json({ message: 'Weapon berhasil dihapus' });
+      } catch (err) {
+          res.status(500).json({ message: err.message });
+      }
+  });
+
 module.exports = router;
