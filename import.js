6e336bd1aecb47d31577754bb1c75e54e78c8cea
Samuel <samuel.535240044@stu.untar.ac.id>
2025-04-24 21:46:17 +0700

    Update untuk import data magicitem ke MongoDB

--- import.js
+++ import.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
-const Class = require('./models/Class');
+const Magicitem = require('./models/Magicitem');
 
 mongoose.connect('mongodb+srv://Samuel:popapopi17@contohcluster.27tfw.mongodb.net/?retryWrites=true&w=majority&appName=contohCluster', {
     useNewUrlParser: true,
@@ -8,10 +8,10 @@ mongoose.connect('mongodb+srv://Samuel:popapopi17@contohcluster.27tfw.mongodb.ne
 .then(async () => {
     console.log('Connected to MongoDB');
     
-    const data = fs.readFileSync('./data/classes.json');
-    const classes = JSON.parse(data);
+    const data = fs.readFileSync('./data/magicitems.json');
+    const magicitems = JSON.parse(data);
 
-    await Class.insertMany(classes);
+    await Magicitem.insertMany(magicitems);
     console.log('Data imported successfully');
 
     mongoose.disconnect();
